be0ca587fdd458104b20161b59d9c7837390ea59
ralucacocioban <zcabrco@live.ucl.ac.uk>
2014-03-24 15:37:19 +0000

    first commit all pages done

---
+++ header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>HealthPassport</title>
+<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css">
+<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap-theme.min.css">
+<script type="text/javascript" src="http://code.jquery.com/jquery.min.js"></script>
+<script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
+<script src="js/manage.js"></script>
+<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+<div class="bs-example">
+	<ul class="nav nav-tabs" id="tabs">
+        <li class="active"><a data-toggle="tab" href="#home"><span class="glyphicon glyphicon-home"></span>  Home</a></li>
+        <li><a data-toggle="tab" href="#healthpass"><span class="glyphicon glyphicon-book"></span> Health Passport</a></li>
+        <li><a data-toggle="tab" href="#forum"><span class="glyphicon glyphicon-envelope"></span> Forum</a></li>
+    </ul>
+</div>
---
+++ index.php
@@ -0,0 +1,80 @@
+<?php
+include "header.php";
+?>    
+<div class="tab-content">
+    <div id="home" class="tab-pane fade in active">
+
+      <div id="myCarousel" class="carousel slide" data-interval="4000" data-ride="carousel">
+        <!-- Carousel indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+        </ol>   
+        <!-- Carousel items -->
+        <div class="carousel-inner">
+            <div class="active item">
+                <h2>Health Passport</h2>
+                <img src="img/image003.jpg"> 
+                <div class="carousel-caption">
+                    <h3>NHS and UCL project</h3>
+
+
+                </div>
+            </div>
+            <div class="item">
+                <h2>About HealthPassport</h2>
+                <div class="carousel-caption">
+                  <h3>something here</h3>
+                  <p>Helping People with Learning Disabilities</p>
+              </div>
+          </div>
+          <div class="item">
+            <h2></h2>
+            <div class="carousel-caption">
+              <h3>Bring Inovation to their world</h3>
+              <p>healthcare data, allergies, reminder system, emotions expression</p>
+          </div>
+      </div>
+  </div>
+  <!-- Carousel nav -->
+  <a class="carousel-control left" href="#myCarousel" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+</a>
+<a class="carousel-control right" href="#myCarousel" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+</a>
+</div>
+
+
+</div>
+
+
+<div id="healthpass" class="tab-pane fade">
+    <h3>HealthPassport</h3>
+    <p>HealthPassport is an application specially developed for people woth learning disabilities and dementia.</p>
+    <p>Project report in the form of a UCL website due Wednesday 30th April. It should have at minimum,
+Front page with product features
+SDK/collaboration opportunities for further development
+video of final product
+requirements background
+development plan, iterations and forks in prototypes
+architectural diagrams
+technical achievements, implementation details, use of design patterns
+management of the project including work packages completed between the team members
+referenced materials cited and examples/trials made
+testing and evaluation of the product solution
+system manual and user manual
+community forum
+</p>
+</div>
+<div id="forum" class="tab-pane fade">
+
+    <a id="nabblelink" href="http://healthpassport.1109320.n5.nabble.com/">healthpassport</a>
+    <script src="http://healthpassport.1109320.n5.nabble.com/embed/f1"></script>
+
+</div>
+</div>
+
+</body>
+</html>    
